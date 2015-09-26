17de5638d0c3d81cbb5c7a1fda17054553f13096
robo <robu1691@gmail.com>
2015-09-26 12:14:21 +0530

    Added Settings menu in DetailFragment

--- app/src/main/java/com/example/robo/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/robo/sunshine/DetailActivity.java
@@ -29,30 +29,6 @@ public class DetailActivity extends ActionBarActivity {
         }
     }
 
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
-    }*/
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -99,6 +75,16 @@ public class DetailActivity extends ActionBarActivity {
             } else {
                 Log.d(LOG_TAG, "Share Action Provider is null?");
             }
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.action_settings) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
         }
 
         private Intent createShareForecastIntent() {
