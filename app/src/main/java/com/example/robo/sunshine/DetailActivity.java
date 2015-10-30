90d1a1442998edd832304a8cf2a5f95d720d7a1a
robo <robu1691@gmail.com>
2015-10-30 11:14:09 +0530

    Updated ActionBarActivity to  AppCompatActivity

--- app/src/main/java/com/example/robo/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/robo/sunshine/DetailActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +16,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-public class DetailActivity extends ActionBarActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
