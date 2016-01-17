f1b11cf416d33a02b9826e3cea9be3982ce58a8d
robo <robu1691@gmail.com>
2016-01-17 12:12:15 +0530

    Added service

--- app/src/main/java/com/example/robo/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/robo/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.robo.sunshine;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.robo.sunshine.data.WeatherContract;
+import com.example.robo.sunshine.service.SunshineService;
 
 public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -162,9 +164,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
