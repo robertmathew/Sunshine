faa3753ae602a5e42ef63713826ddbef78c6f11a
robo <robu1691@gmail.com>
2016-02-29 15:05:55 +0530

    Add a Muzei extension

--- app/src/main/java/com/example/robo/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/robo/sunshine/sync/SunshineSyncAdapter.java
@@ -35,6 +35,7 @@ import com.example.robo.sunshine.MainActivity;
 import com.example.robo.sunshine.R;
 import com.example.robo.sunshine.Utility;
 import com.example.robo.sunshine.data.WeatherContract;
+import com.example.robo.sunshine.muzei.WeatherMuzeiSource;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -369,6 +370,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                         new String[]{Long.toString(dayTime.setJulianDay(julianStartDay - 1))});
 
                 updateWidgets();
+                updateMuzei();
                 notifyWeather();
             }
 
@@ -388,6 +390,16 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
         Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED)
                 .setPackage(context.getPackageName());
         context.sendBroadcast(dataUpdatedIntent);
+    }
+
+    private void updateMuzei() {
+        // Muzei is only compatible with Jelly Bean MR1+ devices, so there's no need to update the
+        // Muzei background on lower API level devices
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Context context = getContext();
+            context.startService(new Intent(ACTION_DATA_UPDATED)
+                    .setClass(context, WeatherMuzeiSource.class));
+        }
     }
 
     private void notifyWeather() {
